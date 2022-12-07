455e86e334cabcdac3ffdf0c0cf39d82e7627b48
wtbtucker <wtbtucker@gmail.com>
2022-12-07 16:16:40 -0500

    Enter order numbers and barcodes into RICS for each store. 99 and 17 removed

--- rma.py
+++ rma.py
@@ -9,13 +9,14 @@ import os
 import csv
 
 driver = webdriver.Edge()
-store_names = dict(Norwell = 1,
+store_names  = dict(Norwell = 1,
                         Boston = 2,
                         Wellesley = 3,
                         Brookline = 4,
                         Cambridge = 5,
                         Melrose = 6,
                         Mansfield = 7,
+                        Warehouse = 8,
                         Branford = 10,
                         Old_Saybrook = 11,
                         Plymouth = 12,
@@ -27,19 +28,23 @@ store_names = dict(Norwell = 1,
                         Manchester = 18,
                         Concord = 19,
                         Northampton = 20,
-                        Canton = 96)
+                        Canton = 96,
+                        OnlineSales = 99)
 
 def main():
     global driver
     login()
     navigate_store('1') # navigate to store 1 for the RMA menu to appear
-    nagivate_rma()
-    print(store_names)
-    
-    # navigate to each store in the list of stores
-    # create a list of barcodes associated with each store
-    #
-
+    navigate_rma()
+    store_dict = create_store_dict()
+    for store in store_dict:
+        navigate_store(store)
+        navigate_menu('ctl00_ContentPlaceHolder1_btnScanView')
+        for line_item in store_dict[store]:
+            driver.find_element(By.ID, 'ctl00_ContentPlaceHolder1_txtRmaNumber').send_keys(line_item[0])
+            driver.find_element(By.ID, 'ctl00_ContentPlaceHolder1_productEntryList_txtIdentifier').send_keys(line_item[1])
+            driver.find_element(By.ID, 'ctl00_ContentPlaceHolder1_productEntryList_txtIdentifier').send_keys(Keys.RETURN)
+            navigate_menu('ctl00_cmdMaster5')
     driver.quit()
 
 def login():
@@ -60,12 +65,12 @@ def navigate_store(store_number):
     storeInputElement.send_keys(Keys.RETURN)
     WebDriverWait(driver, timeout=10).until(EC.text_to_be_present_in_element((By.ID, 'ctl00_lblOrganization'), store_number))
 
-def nagivate_rma():
-    nagivate_menu('ctl00_btnStock')
-    nagivate_menu('n3')
-    nagivate_menu('n14')
+def navigate_rma():
+    navigate_menu('ctl00_btnStock')
+    navigate_menu('n3')
+    navigate_menu('n14')
 
-def nagivate_menu(id):
+def navigate_menu(id):
     try:
         element = driver.find_element(By.ID, id)
         actions = ActionChains(driver)
@@ -80,7 +85,8 @@ def nagivate_menu(id):
         actions.perform()
 
 def create_store_dict():
-    global store_names
+    # initialize dictionary
+    store_dict = {}
 
     # open the orders csv file and create reader object
     path = os.path.dirname(os.path.realpath(__file__))
@@ -92,6 +98,7 @@ def create_store_dict():
         for row in reader:
             store = row["Note"]
             barcode = row["Product barcode"]
+            order_number = row["Order number"]
 
             # convert quantity to integer if not blank
             if row["Line item quantity"]:
@@ -99,7 +106,15 @@ def create_store_dict():
             else:
                 quantity = 0
 
-            
-
+            # append barcode to list quantity number of times
+            while quantity > 0:
+                if store not in store_dict:
+                    store_dict[store] = []
+                store_tuple = (order_number, barcode)
+                store_dict[store].append(store_tuple)
+                quantity = quantity - 1
+    store_dict.pop('99')
+    store_dict.pop('17')
+    return store_dict
 
 main()
