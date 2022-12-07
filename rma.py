a00c8d8dc3e9afbac665849134f6cdd7c82fb357
wtucker <wtbtucker@gmail.com>
2022-12-07 14:52:08 -0500

    Create function for opening the orders file and iterating through the rows

--- rma.py
+++ rma.py
@@ -5,16 +5,41 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.common.exceptions import StaleElementReferenceException 
-import time
+import os
+import csv
 
 driver = webdriver.Edge()
+store_names = dict(Norwell = 1,
+                        Boston = 2,
+                        Wellesley = 3,
+                        Brookline = 4,
+                        Cambridge = 5,
+                        Melrose = 6,
+                        Mansfield = 7,
+                        Branford = 10,
+                        Old_Saybrook = 11,
+                        Plymouth = 12,
+                        Yarmouth = 13,
+                        Shrewsbury = 14,
+                        Glastonbury = 15,
+                        Fairfield = 16,
+                        Portsmouth = 17,
+                        Manchester = 18,
+                        Concord = 19,
+                        Northampton = 20,
+                        Canton = 96)
 
 def main():
     global driver
     login()
-    navigate_store('1')
+    navigate_store('1') # navigate to store 1 for the RMA menu to appear
     nagivate_rma()
-    time.sleep(10)
+    print(store_names)
+    
+    # navigate to each store in the list of stores
+    # create a list of barcodes associated with each store
+    #
+
     driver.quit()
 
 def login():
@@ -53,4 +78,28 @@ def nagivate_menu(id):
         actions.move_to_element(element)
         actions.click(element)
         actions.perform()
+
+def create_store_dict():
+    global store_names
+
+    # open the orders csv file and create reader object
+    path = os.path.dirname(os.path.realpath(__file__))
+    orders_path = path + "\\orders.csv"
+    with open(orders_path, "r") as o:
+        reader = csv.DictReader(o)
+
+        # iterate over reader and create dictionary with stores as keys and barcodes as values
+        for row in reader:
+            store = row["Note"]
+            barcode = row["Product barcode"]
+
+            # convert quantity to integer if not blank
+            if row["Line item quantity"]:
+                quantity = int(row["Line item quantity"])
+            else:
+                quantity = 0
+
+            
+
+
 main()
